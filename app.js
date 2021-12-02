730e1dd08a798ca5716ecc3ffd6984e07580ec5d
Simon Sherbinin <simonshrb@gmail.com>
2021-12-02 23:29:36 +0200

    reviewer fixes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
@@ -7,6 +8,7 @@ const cardsRouter = require("./routes/cards");
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/arounddb");
