4a52ef2c6db61326f6a56c486c631a8ae18654a9
Simon Sherbinin <simonshrb@gmail.com>
2021-12-18 23:13:54 +0200

    removed options again

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(cors());
-app.options("*", cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
