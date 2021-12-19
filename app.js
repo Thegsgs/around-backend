ccdd3fe818b9f9a7ba81cfe5af0e46af8bc871f9
Simon Sherbinin <simonshrb@gmail.com>
2021-12-19 15:21:32 +0200

    changed code order

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.options("*", cors());
+
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
@@ -10,9 +15,6 @@ const { requestLogger, errorLogger } = require("./middleware/logger");
 
 const { PORT = 3000 } = process.env;
 
-const app = express();
-app.use(cors());
-app.options("*", cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
