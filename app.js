2882de6608d0ebb99868dcfc5eb70027d0c63d68
Simon Sherbinin <simonshrb@gmail.com>
2021-12-19 14:23:48 +0200

    added cors as middleware

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/arounddb");
 app.use(requestLogger);
-app.use("/", usersRouter, cardsRouter);
+app.use("/", cors(), usersRouter, cardsRouter);
 
 app.use(errorLogger);
 app.use(errors());
