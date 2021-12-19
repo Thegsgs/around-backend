7792a7b86df98bf4eef4f2feb148b18f9ae166ba
Simon Sherbinin <simonshrb@gmail.com>
2021-12-19 14:10:04 +0200

    noob 2

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ const { requestLogger, errorLogger } = require("./middleware/logger");
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(helmet());
 app.use(cors());
 app.options("*", cors());
-
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost:27017/arounddb");
